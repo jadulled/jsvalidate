4ec7a0d1c707768627de6db7b0fa11b97fa412c4
Marcos Medrano <marcos@zaubersoftware.com>
2008-11-20 17:36:00 +0000

    Refactor:  - Agrego documentacion.  - Separo el codigo que se genera por error de validacion para poder sobreescribirlo.

--- src/main/javascript/validate.js
+++ src/main/javascript/validate.js
@@ -18,11 +18,13 @@
  */
 
 /**
- * DefensiveJS
- *
+ * DefensiveJS.
+ * @file validate.js
+ * 
  * @version 0.0-SNAPSHOT
  *
  * @fileOverview Javascript validation library to perform Defensive Programming. (inspired on org.apache.commons.lang.Validate)
+ *
  * @author Marcos J. Medrano <marcos [at] zauber [dot] com [dot] ar>
  * @see <a href="http://commons.apache.org/lang/apidocs/org/apache/commons/lang/Validate.html">org.apache.commons.lang.Validate</a>.
  * @see <a href="http://en.wikipedia.org/wiki/Defensive_programming">Defensive Programming</a>
@@ -40,16 +42,6 @@ var validate = {};
 var NULL_VALUE = null;
 var UNDEFINED_VALUE = undefined;
 
-// The error that is throwed when validation fails.
-var IllegalArgumentError = Error;
-
-
-/** Generic function that validates an object if equals a value.
- * Throws IllegalArgumentError if the obj equals the value. */
-var validate_not_value = function(obj, value, msg){
-	if (obj === value)
-		throw new IllegalArgumentError(msg);
-}
 
 /** Validate object constructor */
 var Validate = function(){};
@@ -58,27 +50,29 @@ Validate.prototype = {
 	/** Validate an argument, throwing IllegalArgumentError if the argument is null.
 	 * Optionally it takes a message string argument for the error message. */
 	notNull: function(obj, msg){
-		validate_not_value(obj, NULL_VALUE, msg || "Expected a not null argument.");
+		if (obj === NULL_VALUE)
+			this.throwError(msg || "Expected a not null argument.");
 	},
 
 	/** Validate an argument, throwing IllegalArgumentError if the argument is undefined.
 	 * Optionally it takes a message string argument for the error message. */
 	notUndefined: function(obj, msg){
-		validate_not_value(obj, UNDEFINED_VALUE, msg || "Expected a not undefined argument."); 
+		if (obj === UNDEFINED_VALUE)
+			this.throwError(msg || "Expected a not undefined argument.");
 	},
 
 	/** Validate an argument, throwing IllegalArgumentError if the argument is not defined.
 	 * Optionally it takes a message string argument for the error message. */
 	isDefined: function(obj, msg){
-		validate_not_value(obj, UNDEFINED_VALUE, msg || 'Expected a defined argument.');
-		validate_not_value(obj, NULL_VALUE, msg || 'Expected a defined argument.');
+		this.notNull(obj, msg || "Expected a defined argument.");
+		this.notUndefined(obj, msg || "Expected a defined argument.")
 	},
 
 	/** Validate an argument, throwing IllegarArgumentError if the argument is not a function.
 	 *  Optionally it takes a message string argument for the error message. */
 	isFunction: function(func, msg){
 		if (typeof(func) != typeof(function (){}))
-			throw new IllegalArgumentError(msg || "Expected a function argument.");
+			this.throwError(msg || "Expected a function argument.");
 	},
 
 	/** Validate an argument, throwing IllegalArgumentError if the object does not have the property prop.
@@ -94,16 +88,20 @@ Validate.prototype = {
 		this.isDefined(arr, msg || 'Expected a defined array.');
 		this.hasProperty(arr,'length', msg || 'Expected an array argument.');
 		if (arr.length == 0)
-			throw new IllegalArgumentError(msg || 'Expected a not empty array.');
+			this.throwError(msg || 'Expected a not empty array.');
 	},
 
 	/** Validate an array, throwing IllegalArgumentError if the array contains a null element.
 	 * Optionally it takes a message string argument for the error message. */
 	definedElements: function(arr, msg){
 		this.isDefined(arr); // validate is defined
-		for(var i in arr){
-			validate_not_value(arr[i], NULL_VALUE, msg || 'Expected an array with no null elements');
-		}
+		for(var i in arr)
+			this.notNull(arr[i],msg || 'Expected an array with no null elements');
+	},
+	throwError: function(errorMessage){
+		var error = new Error(errorMessage);
+		error.name = "IllegalArgumentError";
+		throw error;
 	}
 }
 
@@ -120,7 +118,7 @@ Attribute.prototype = {};
 
 // "Export" Validate class
 validate.Validate = new Validate();
-validate.IllegalArgumentError = IllegalArgumentError;
+//validate.IllegalArgumentError = IllegalArgumentError;
 
 })();
 
